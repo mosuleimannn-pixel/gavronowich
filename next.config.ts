35496d3f48908ae24454945a30872e10620dfd07
Mosul <mosul@MacBook-Pro-von-Mosul-3.local>
2026-03-15 12:44:31 +0100

    Remove broken Pages config, use Vercel instead

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/gavronowich',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default withNextIntl(nextConfig);
